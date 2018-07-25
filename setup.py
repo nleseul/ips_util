575be751eedd9117fa5b77c8978b333f3aac8a86
nleseul <nleseul@this-life.us>
2018-07-25 19:30:32 -0400

    Package scaffolding

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ips_util',
+    version='0.0',
+    description='ips_util',
+    url='https://github.com/nleseul/ips_util',
+    author='NLeseul',
+    license='Unlicense',
+    packages=['ips_util'],
+    zip_safe=False)
